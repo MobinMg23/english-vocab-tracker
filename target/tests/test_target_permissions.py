fd6012c1034f3214f96743b1c5bb8d227b153b9e
mobin <mobin.1383.goodarziyan@gmail.com>
2025-07-12 17:09:25 -0700

    edite IsOwner Permission Test 4

--- target/tests/test_target_permissions.py
+++ target/tests/test_target_permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APITestCase, APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 from target.models import LearningTarget
 from django.urls import reverse 
@@ -7,13 +8,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+def create_user_jwt_token(user):
+    refresh = RefreshToken.for_user(user=user)
+    return str(refresh.access_token)
+
 
 class IsOwnerPermissionTests(APITestCase):
     def setUp(self):
 
         self.owner_user = User.objects.create_user(username='owner', password='okokok')
         self.other_user = User.objects.create_user(username='vini', password='jr')
-        self.client = APIClient()
 
         self.target = LearningTarget.objects.create(
             user=self.owner_user,
@@ -24,13 +28,15 @@ class IsOwnerPermissionTests(APITestCase):
         self.url = reverse("target-detail", kwargs={'pk': self.target.id})
 
     def test_owner_can_access(self):
-        self.client.login(username='owner', password='okokok')
+        token = create_user_jwt_token(self.owner_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_not_owner(self):
-        self.client.login(username='vini', password='jr')
+        token = create_user_jwt_token(self.other_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
